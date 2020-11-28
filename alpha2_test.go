67f6acf9af4bf64e2319f3d4e74a3a4ac87a1d36
James Williams <jamesphwilliams@gmail.com>
2020-11-28 18:55:21 +0000

    Add benchmarking

---
+++ alpha2_test.go
@@ -0,0 +1,27 @@
+package iso3166
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/biter777/countries"
+)
+
+func BenchmarkOurAlpha2(b *testing.B) {
+	rand.Seed(time.Now().Unix())
+	for n := 0; n < b.N; n++ {
+		country := AllCountries[rand.Intn(len(AllCountries))]
+		_ = Alpha2(country)
+	}
+}
+
+var theirAllCountries = countries.All()
+
+func BenchmarkTheirAlpha2(b *testing.B) {
+	rand.Seed(time.Now().Unix())
+	for n := 0; n < b.N; n++ {
+		country := theirAllCountries[rand.Intn(len(theirAllCountries))]
+		_ = country.Alpha2()
+	}
+}
